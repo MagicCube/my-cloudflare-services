e98d237aa7259d164bdd226e8c35e66ffa6b7aa4
亨睿 <henry.lx@alibaba-inc.com>
2020-02-11 14:23:21 +0800

    handle stock and weather

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { handleIndex } from './handlers';
+import { handleIndex, handleStock, handleWeather } from './handlers';
 
 import Router from './router';
 
@@ -9,6 +9,8 @@ addEventListener('fetch', event => {
 async function handleRequest(request: Request) {
   const router = new Router();
   router.get('/', handleIndex);
+  router.get('/stock', handleStock);
+  router.get('/weather', handleWeather);
   let response = await router.route(request);
   if (!response) {
     response = new Response('Not found', { status: 404 });
